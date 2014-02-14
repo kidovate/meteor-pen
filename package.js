6674c4ccd279c2c99936c67c310882ee42d3d4d2
Christian Stewart <kidovate@gmail.com>
2014-02-14 08:12:17 -0800

    Move pen source files directly into the repo

--- package.js
+++ package.js
@@ -4,11 +4,11 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.add_files(['lib/src/font/fontello.eot', 'lib/src/font/fontello.svg', 'lib/src/font/fontello.ttf', 'lib/src/font/fontello.woff'], 'client')
-  api.add_files('lib/src/pen.css', 'client');
-  api.add_files('lib/src/pen.js', 'client');
-  api.add_files('lib/src/markdown.js', 'client');
+  api.add_files(['src/font/fontello.eot', 'src/font/fontello.svg', 'src/font/fontello.ttf', 'lib/src/font/fontello.woff'], 'client')
+  api.add_files('src/pen.css', 'client');
+  api.add_files('src/pen.js', 'client');
+  api.add_files('src/markdown.js', 'client');
 
-  // This makes the paths to the fonts absolute
+  / This makes the paths to the fonts absolute
   api.add_files('override.css', 'client');
 });
